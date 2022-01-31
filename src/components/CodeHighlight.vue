ee363acd9d03a91d52e357c374c183870923a1c4
murmel <63870352+murme1@users.noreply.github.com>
2022-01-31 23:57:45 +0100

    hints for code

--- src/components/CodeHighlight.vue
+++ src/components/CodeHighlight.vue
@@ -1,37 +1,105 @@
 <script lang="ts" setup>
-import { ref, h } from 'vue'
+import { computed, ref } from 'vue'
 
 const props = defineProps<{
   code: string,
 }>()
 
-const codeLines = ref(props.code.split('\n').map((line) => line.trim()).filter(Boolean))
+const showHints = ref(false)
+
+const codeLines = computed(() => {
+  return props.code.split('\n')
+    .map((line) => line.trim())
+    .filter(Boolean)
+    .filter((line) => !line.startsWith('#') || showHints.value)
+})
 </script>
 
 <template>
-  <pre>
-    <span v-for="line in codeLines">{{ line }}</span>
-  </pre>
+  <div>
+    <div class="intro">
+      <small v-if="showHints">
+        Mit <code>Ctrl</code>+<code>Shift</code>+<code>V</code>
+        kannst du diese Zeilen in ein Terminal einfügen.
+      </small>
+      <label
+        class="switch"
+        title="Wenn du dir unsicher bist, was diese Zeilen bedeuten, kannst du dir hiermit Erläuterungen anzeigen lassen."
+      >
+        Erläuterungen anzeigen
+        <input
+          type="checkbox"
+          v-model="showHints"
+        />
+      </label>
+    </div>
+    <pre>
+      <span
+        v-for="line in codeLines"
+        v-html="line"
+        :class="line.startsWith('#') ? 'comment' : 'line'"
+      />
+    </pre>
+  </div>
 </template>
 
 <style scoped>
-pre {
+div {
   padding: 1rem;
-  counter-reset: line;
-  overflow-y: auto;
   max-height: 100%;
+}
+
+.intro {
+  margin-bottom: 1rem;
+  display: grid;
+  grid-template-columns: 1fr auto;
+  align-items: start;
+}
+
+.switch {
+  display: block;
+  font-size: 0.8em;
+  background-color: #ffffff20;
+  border-radius: 10px;
+  padding: 0.3rem 0.5rem;
+  justify-self: right;
+}
+
+.switch input {
+  color: var(--primary-color);
+}
+
+pre {
+  margin-top: 2rem;
+  counter-reset: line;
   white-space: normal;
 }
-pre span {
+
+small,
+.comment,
+.switch {
+  color: #ccc;
+}
+
+.comment,
+.line {
   display: block;
-  position: relative;
-  line-height: 1.5rem;
-  white-space: normal;
   padding-left: 2.2em;
   line-height: 1.5;
+}
+
+.comment {
+  user-select: none;
+  margin-top: 0.2em;
+  font-size: 0.8em;
+}
+
+.line {
+  position: relative;
+  white-space: normal;
   margin-bottom: 0.5em;
 }
-pre span:before {
+.line:before {
   counter-increment: line;
   content: counter(line);
   position: absolute;
@@ -40,6 +108,6 @@ pre span:before {
   display: block;
   border-right: 1px solid #ddd;
   padding: 0 .5em;
-  color: #888;
+  color: #999;
 }
 </style>
--- src/components/guides/DesktopUbuntuStep.vue
+++ src/components/guides/DesktopUbuntuStep.vue
@@ -2,14 +2,19 @@
 import CodeHighlight from '../CodeHighlight.vue'
 
 const code = `
+  # Mit <code>sudo</code> führst du den nachfolgenden Befehl als Administrator:in aus. <code>apt install</code> installiert Programme.
   sudo apt install -y wget apt-transport-https
+  # Programme in Linux werden mit GPG signiert, um sicherzustellen, dass wirklich das drin steckt, was drauf steht. Der nachfolgende Befehl lädt den passenden GPG Key herunter.
   sudo wget -O /usr/share/keyrings/element-io-archive-keyring.gpg https://packages.element.io/debian/element-io-archive-keyring.gpg
+  # Programme werden über sog. "Paketquellen" bereit gestellt. Der Befehl für die Element Paketquelle zu deinem System hinzu.
   echo "deb [signed-by=/usr/share/keyrings/element-io-archive-keyring.gpg] https://packages.element.io/debian/ default main" | sudo tee /etc/apt/sources.list.d/element-io.list
+  # Hiermit werden die Paketquellen aktualisiert..
   sudo apt update
+  # ..um Element schließlich zu installieren.
   sudo apt install element-desktop
 `
 </script>
 
 <template>
-  <CodeHighlight :code="code" />
+  <CodeHighlight :code="code" v-bind="$attrs" />
 </template>
